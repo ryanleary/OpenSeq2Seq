bae7c949a3f136027fa2b0c7903935ddc5acc18f
Ryan Leary <rleary@nvidia.com>
2019-02-21 15:01:37 -0500

    Add min_duration optional data filter

--- open_seq2seq/data/speech2text/speech2text.py
+++ open_seq2seq/data/speech2text/speech2text.py
@@ -37,6 +37,7 @@ class Speech2TextDataLayer(DataLayer):
         'augmentation': dict,
         'pad_to': int,
         'max_duration': float,
+        'min_duration': float,
         'bpe': bool,
         'autoregressive': bool,
         'syn_enable': bool,
@@ -122,6 +123,7 @@ class Speech2TextDataLayer(DataLayer):
     self._iterator = None
     self._input_tensors = None
 
+    self.params['min_duration'] = params.get('min_duration', -1.0)
     self.params['max_duration'] = params.get('max_duration', -1.0)
     self.params['window_size'] = params.get('window_size', 20e-3)
     self.params['window_stride'] = params.get('window_stride', 10e-3)
@@ -166,6 +168,11 @@ class Speech2TextDataLayer(DataLayer):
           self._dataset = self._dataset.filter(
               lambda x, x_len, y, y_len, duration:
               tf.less_equal(duration, self.params['max_duration'])
+          )
+        if self.params['min_duration'] > 0:
+          self._dataset = self._dataset.filter(
+            lambda x, x_len, y, y_len, duration:
+            tf.greater_equal(duration, self.params['min_duration'])
           )
         self._dataset = self._dataset.map(
             lambda x, x_len, y, y_len, duration:
